d7251e29c14673729f315862f2077c1e1580e326
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-29 14:03:47 +0200

    Completamento esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che 
 */
 
 
-// creiamo una lista spesa (array)
+// Lista spesa (array)
 let listaSpesa = [
     `Comprare il pane`,
     `Comprare il latte`,
@@ -17,3 +17,22 @@ let listaSpesa = [
     `Passare dalla nonna `,
     `Prendere i figli a scuola `
 ]
+
+// Indice con valore a zero
+let indice = 0
+
+// Creazione ciclo while con la condizione che l'indice dovrà essere sempre più piccolo della lunghezza dell'array
+while (indice < listaSpesa.length) {
+
+    // A ogni ciclo inseriamo questi dati nell'elemento div(#lista)
+    document.getElementById(`lista`).innerHTML +=
+    ` 
+    <li>${listaSpesa[indice]}</li>
+    
+    `
+    // Dopo ogni giro l'indice dovrà essere aumentato di 1
+    indice++;
+
+    console.log(listaSpesa[indice])
+    
+}
