ff611bc198c053eed2846d1c2028cb970ec36b9f
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-03-29 14:00:44 +0200

    Preparazione struttura Html e lista array nel file Javascript

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/*
+Consegna:
+Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+*/
+
+
+// creiamo una lista spesa (array)
+let listaSpesa = [
+    `Comprare il pane`,
+    `Comprare il latte`,
+    `Comprare le carote `,
+    `Comprare la carne `,
+    `Comprare il pollo `,
+    `Comprare i croccantini per il cane `,
+    `Comprare le padelle `,
+    `Prendere i vestiti in lavanderia `,
+    `Passare dalla nonna `,
+    `Prendere i figli a scuola `
+]
